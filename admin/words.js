00fdee2da6d808a6e62582c9b3c8f3ec5eec3c29
Thomas OEDING <thomas.oeding@gmail.com>
2019-06-13 22:26:45 +0200

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"ryd adapter settings": {
+		"en": "Adapter settings for ryd",
+		"de": "Adaptereinstellungen für ryd",
+		"ru": "Настройки адаптера для ryd",
+		"pt": "Configurações do adaptador para ryd",
+		"nl": "Adapterinstellingen voor ryd",
+		"fr": "Paramètres d'adaptateur pour ryd",
+		"it": "Impostazioni dell'adattatore per ryd",
+		"es": "Ajustes del adaptador para ryd",
+		"pl": "Ustawienia adaptera dla ryd",
+		"zh-cn": "ryd的适配器设置"
+	},
+	"option1": {
+		"en": "option1",
+		"de": "Option 1",
+		"ru": "Опция 1",
+		"pt": "Opção 1",
+		"nl": "Optie 1",
+		"fr": "Option 1",
+		"it": "opzione 1",
+		"es": "Opción 1",
+		"pl": "opcja 1",
+		"zh-cn": "选项1"
+	},
+	"option2": {
+		"en": "option2",
+		"de": "Option 2",
+		"ru": "option2",
+		"pt": "opção 2",
+		"nl": "Optie 2",
+		"fr": "Option 2",
+		"it": "opzione 2",
+		"es": "opcion 2",
+		"pl": "Opcja 2",
+		"zh-cn": "选项2"
+	}
+};
